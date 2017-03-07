6a86195378237a6290e5e8a5c40c24829514a5d5
Gummi <geb12@hi.is>
2017-03-07 15:58:50 +0100

    Move leagueItems to it's own component

---
+++ app/components/LeagueItems.jsx
@@ -0,0 +1,51 @@
+import React, { PropTypes } from 'react';
+import classNames from 'classnames/bind';
+import _ from 'lodash';
+
+import nbaImage from '../images/nba-logo-transparent-png-logo-download.png';
+import styles from '../css/components/leagueItem.css';
+
+
+const cx = classNames.bind(styles);
+
+// TODO MOVE ALL THIS TO DIFFRIENT COMPONENTS
+const LeagueItems = ({fantasyLeagues}) => {
+  return (
+    // TODO move to component
+    <div className={cx('leagueItemsWraper')}>
+      {fantasyLeagues && fantasyLeagues !== 'error' && fantasyLeagues.map(league => {
+        return (
+          <div className={cx('leagueItemWraper')}>
+            <div className={cx('leagueItemHeader')}>
+              <h1 className={cx('leagueItemHeaderText')}>{league.name}</h1>
+              <div className={cx('leagueItemImageWraper')}>
+                <img className={cx('leagueItemImage')} src={nbaImage} />
+              </div>
+            </div>
+            <div className={cx('leagueItemTextWraper')}>
+              <div className={cx('leagueItemText')}>
+                <p><span className={cx('leagueItemTextHeader')}>Teams</span></p>
+                <span className={cx('leagueItemTextSubHeader')}>{league.num_teams}</span>
+              </div>
+              <div className={cx('leagueItemText')}>
+                <p><span className={cx('leagueItemTextHeader')}>Type</span></p>
+                <span className={cx('leagueItemTextSubHeader')}>{league.scoring_type}</span>
+              </div>
+              <div className={cx('leagueItemText')}>
+                <p><span className={cx('leagueItemTextHeader')}>Season</span></p>
+                <span className={cx('leagueItemTextSubHeader')}>{league.season}</span>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+LeagueItems.propTypes = {
+  fantasyLeagues: PropTypes.object.isRequired
+
+};
+
+export default LeagueItems;
--- app/components/MainDashboardSection.jsx
+++ app/components/MainDashboardSection.jsx
@@ -3,44 +3,16 @@ import classNames from 'classnames/bind';
 import { List } from 'material-ui/List';
 import _ from 'lodash';
 
+import LeagueItems from './LeagueItems';
+
 import nbaImage from '../images/nba-logo-transparent-png-logo-download.png';
 import styles from '../css/components/leagueItem.css';
 
-
 const cx = classNames.bind(styles);
 
-// TODO MOVE ALL THIS TO DIFFRIENT COMPONENTS
 const MainDashboardSection = ({fantasyLeagues}) => {
   return (
-    // TODO move to component
-    <div className={cx('leagueItemsWraper')}>
-      {fantasyLeagues && fantasyLeagues !== 'error' && fantasyLeagues.map(league => {
-        return (
-          <div className={cx('leagueItemWraper')}>
-            <div className={cx('leagueItemHeader')}>
-              <h1 className={cx('leagueItemHeaderText')}>{league.name}</h1>
-              <div className={cx('leagueItemImageWraper')}>
-                <img className={cx('leagueItemImage')} src={nbaImage} />
-              </div>
-            </div>
-            <div className={cx('leagueItemTextWraper')}>
-              <div className={cx('leagueItemText')}>
-                <p><span className={cx('leagueItemTextHeader')}>Teams</span></p>
-                <span className={cx('leagueItemTextSubHeader')}>{league.num_teams}</span>
-              </div>
-              <div className={cx('leagueItemText')}>
-                <p><span className={cx('leagueItemTextHeader')}>Type</span></p>
-                <span className={cx('leagueItemTextSubHeader')}>{league.scoring_type}</span>
-              </div>
-              <div className={cx('leagueItemText')}>
-                <p><span className={cx('leagueItemTextHeader')}>Season</span></p>
-                <span className={cx('leagueItemTextSubHeader')}>{league.season}</span>
-              </div>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    <LeagueItems fantasyLeagues={fantasyLeagues}/>
   );
 };
 
