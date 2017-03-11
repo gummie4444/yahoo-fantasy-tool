4fc3447a292897628645185c905bdd203cdef24b
Gummi <geb12@hi.is>
2017-03-11 16:36:16 +0100

    Create new yfObject for each user to allow multiple users to use it at the same time

--- server/init/yahooFantasy/index.js
+++ server/init/yahooFantasy/index.js
@@ -1,7 +1,6 @@
 import YahooFantasy from 'yahoo-fantasy';
 import { yahoo } from '../../../config/secrets';
 
-console.log(YahooFantasy, "yahooFant");
 // you can get an application key/secret by creating a new application on Yahoo!
 let yf = new YahooFantasy(
   yahoo.clientID,
